954545e542944ff16c150dbab92bf4a86ad21cdf
Jhos236 <juanmiguelcrf@gmail.com>
2025-11-12 22:02:17 -0400

    Update service-worker.js
    Add js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,28 @@
-const CACHE_NAME = "sate-pwa-v1";
+const CACHE_NAME = 'sate-v2-cache-v1';
 const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener("install", evt => {
+self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", evt => {
+self.addEventListener('activate', evt => {
   evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => {
+      if(k !== CACHE_NAME) return caches.delete(k);
+    })))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", evt => {
+self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(resp =>
-      resp || fetch(evt.request).catch(() => caches.match("./index.html"))
-    )
+    caches.match(evt.request).then(resp => resp || fetch(evt.request).catch(() => caches.match('./index.html')))
   );
 });
